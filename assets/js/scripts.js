cac7f606df2b4e2c7b485732939234d0b41e1b6f
theonlyrichard <richard.cardoza16@gmail.com>
2022-05-05 21:14:08 -0700

    build

---
+++ assets/js/scripts.js
@@ -0,0 +1,28 @@
+// ARRAY OF FIVE QUESTION 
+const questions = [
+    {
+        question: "Commonly used data types DO Not include:",
+        answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        correctAnswer: "2"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within _______.",
+        answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        correctAnswer: "1"
+    },
+    {
+        question: "Questions 6 : The first index of an array is ____.",
+        choices: ["a. 0", "b. 1", "c. 8", "d. any"],
+        answer: "a"
+    },
+    {
+        question: "String values must be enclosed within ________ when being assigned to variables.",
+        answers: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        correctAnswer: "2"
+    },
+    {
+        question: "Questions 8 : Which event occurs when the user clicks on an HTML element?",
+        choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
+        answer: "a"
+    }
+];
